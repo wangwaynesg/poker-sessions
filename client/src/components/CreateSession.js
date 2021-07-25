6888f0c4346c452536a3d6820adcbf3e4c21d424
Wang Wayne <wangwaynesg@gmail.com>
2021-07-26 02:25:13 +0800

    Fix form submission

--- client/src/components/CreateSession.js
+++ client/src/components/CreateSession.js
@@ -17,22 +17,21 @@ const CreateSession = () => {
 
     const [netProfit, setNetProfit] = useState(0);
 
-    const updateNetProfit = () => {
-        var temp = 0;
-        playerList.forEach(x => {
-            temp = temp + x.playerProfit;
-        })
-        setNetProfit(temp);
-    }
-
     const handlePlayerInputChange = (e, index) => {
         const { name, value } = e.target;
         const list = [...playerList];
         playerList[index][name] = value;
 
         playerList[index]["playerProfit"] = sessionData.buyInAmount * (playerList[index]["playerChips"] / sessionData.chipsPerBuyIn - playerList[index]["playerBuyIns"]);
+        
         setPlayerList(list);
-        updateNetProfit();
+        setSessionData({ ...sessionData, players:list });
+
+        var tempProfit = 0;
+        list.forEach(x => {
+            tempProfit = tempProfit + x.playerProfit;
+        })
+        setNetProfit(tempProfit);
     };
 
     const handlePlayerRemoveClick = (index, e) => {
@@ -41,35 +40,31 @@ const CreateSession = () => {
         const list = [...playerList];
         list.splice(index, 1);
         setPlayerList(list);
+        setSessionData({ ...sessionData, players:list });
         setNetProfit(netProfit - temp);
     };
 
     const handlePlayerAddClick = (e) => {
         e.preventDefault();
-        setPlayerList([...playerList, {
+        const list = [...playerList];
+        list.push({
             playerName: "",
             playerBuyIns: 1,
             playerChips: "",
             playerProfit: -sessionData.buyInAmount
-        }]);
-        setNetProfit(netProfit - 20);
+        });
+
+        setPlayerList(list);
+        setSessionData({ ...sessionData, players:list });
+        setNetProfit(netProfit - sessionData.buyInAmount);
     };
 
     const onSubmit = (e) => {
         e.preventDefault();
 
         console.log(sessionData);
-
         dispatch(createSession(sessionData));
-
-        sessionData.sessionName = "";
-        sessionData.buyInAmount = "";
-        sessionData.chipsPerBuyIn = "";
     };
-
-    const test = () => {
-        console.log(playerList);
-    }
 
     return (
         <div> 
@@ -157,8 +152,6 @@ const CreateSession = () => {
                     <input type="submit" className="btn btn-primary" value="OK"/>
                 </div>
             </form>
-
-            <button onClick={test}>test button</button>
         </div>
     );
 }
